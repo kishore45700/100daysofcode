7fd57161bb7b78385852521ce457ec1271ed20c2
kishore muthuraman <you@example.com>
2021-05-25 22:38:13 +0530

    updated BandNameGenerator.py

--- Day1-BandNameGenerator/BandNameGenerator.py
+++ Day1-BandNameGenerator/BandNameGenerator.py
@@ -1,4 +1,4 @@
-print("Welcome to Band Name Generator.\n")
+print("Welcome to Band Name Generator.")
 #Ask the user for the city where he/she grew in.
 city=input("Which city did you grew up in?\n")
 #Ask the user for the name of their pet
